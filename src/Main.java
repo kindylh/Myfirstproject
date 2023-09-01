80b93a97d028a52c525edbda036919d7ffac79f6
100138808 <kindyl.hogan245@gmail.com>
2023-09-01 08:24:38 -0500

    .

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,20 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.println("       -------            ------- ");
+        System.out.println("        -----              ----- ");
         System.out.println("      ---------          --------- ");
         System.out.println("    -------------      ------------- ");
         System.out.println("   -----------------------------------                 ");
         System.out.println("  -------------------------------------");
         System.out.println(" ---------------------------------------");
         System.out.println("-----------------------------------------");
+        System.out.println(" ---------------------------------------");
+        System.out.println("  -------------------------------------");
+        System.out.println("    --------------------------------- ");
+        System.out.println("       ---------------------------        ");
+        System.out.println("          ---------------------          ");
+        System.out.println("             ---------------           ");
+        System.out.println("                ---------                           ");
+        System.out.println("                   ----               ");
         }
     }
